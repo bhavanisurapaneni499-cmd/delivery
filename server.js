8c1f14391fffa762756adbcdd311659e6d2ca36f
Bhavani Surapaneni <bhavanisurapaneni499@gmail.com>
2026-01-20 13:12:02 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  // Delivery boy sends location
+  socket.on("sendLocation", (data) => {
+    // data = { lat, lng }
+    io.emit("receiveLocation", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("🚀 Server running on http://localhost:5000");
+});
+
